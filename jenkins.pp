d2185c2bfd37ca41742721b55d07ec4b3765ef87
Rich Burroughs <rich@richburroughs.dev>
2025-05-09 16:01:25 -0400

    add comments with Jenkis install procedure

--- jenkins.pp
+++ jenkins.pp
@@ -9,5 +9,17 @@ class jenkins {
 
 
 
+
+# sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+#    https://pkg.jenkins.io/debian/jenkins.io-2023.key
+  
+# echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+#    https://pkg.jenkins.io/debian binary/ | sudo tee \
+#    /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# sudo apt install openjdk-17-jdk
+
+# sudo apt-get install jenkins
+
     
 }
